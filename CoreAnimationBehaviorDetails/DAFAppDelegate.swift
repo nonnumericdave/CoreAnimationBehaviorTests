850ffa3cfaf1bf568273435eb00177af81e7ac33
David Flores <nonnumericdave@gmail.com>
2016-03-20 21:27:09 -0600

    + Clean up basic project.
    -- daf

---
+++ CoreAnimationBehaviorDetails/DAFAppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  DAFAppDelegate.swift
+//  CoreAnimationBehaviorDetails
+//
+//  Created by David Flores on 3/20/16.
+//  Copyright © 2016 David Flores. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class DAFAppDelegate: UIResponder, UIApplicationDelegate
+{
+    var window: UIWindow?
+}
---
+++ CoreAnimationBehaviorDetails/DAFViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DAFViewController.swift
+//  CoreAnimationBehaviorDetails
+//
+//  Created by David Flores on 3/20/16.
+//  Copyright © 2016 David Flores. All rights reserved.
+//
+
+import UIKit
+
+class DAFViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
